e4d7fcf72c00f64e44fc3b58fa3c51625092bf51
horner <horner@mieweb.com>
2022-09-03 18:58:03 -0400

    Added Release notes to readme when packaging

--- build-for-webchart.sh
+++ build-for-webchart.sh
@@ -1,3 +1,7 @@
 :
 
-yarn install && yarn build:package && cp README.webchart.md platform/viewer/dist/README.md && scp platform/viewer/dist/* zeus:wcrc/master/webchart/system/public/ohif
+yarn install && 
+yarn build:package && 
+cp README.webchart.md platform/viewer/dist/README.md && 
+(git status ; git branch -vv ; echo "Release notes:" ; git log --pretty=format:"%h%x09%an%x09%ad%x09%s" | head -n 100) >> platform/viewer/dist/README.md &&
+scp platform/viewer/dist/* zeus:wcrc/master/webchart/system/public/ohif
